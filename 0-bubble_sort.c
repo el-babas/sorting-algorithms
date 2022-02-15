da86c12bfaf0aa509188f72d0c28179d369bc8a6
cristhian1107 <cristhian.cjaa@gmail.com>
2022-02-14 22:19:23 -0500

    Bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,24 +9,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, sz = size;
+	size_t i = 0, j = 0, sz = size - 1;
 	int tmp = 0;
 
-	while (i < (sz - 1))
+	for (i = 0; i < sz; i++)
 	{
-		if (array[i] > array[i + 1])
+		for (j = 0; j < sz - i; j++)
 		{
-			tmp = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = tmp;
-			print_array(array, size);
+			/* Info: At each completion of the loop one less element is taken */
+			if (array[j] > array[j + 1])
+			{
+				/* Info: Swap index */
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
 		}
-		if ((i + 2) == sz)
-		{
-			i = -1;
-			sz--;
-			/* printf(" %ld ------------\n", sz);*/
-		}
-		i++;
 	}
 }
