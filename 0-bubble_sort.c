c4b12b2fe077a8b0df194c5f2fd7385d46233a13
cristhian1107 <cristhian.cjaa@gmail.com>
2022-02-14 21:18:43 -0500

    [Add] Bubble sort  Changes to be committed: 	new file:   .gitignore 	new file:   0-O 	new file:   0-bubble_sort.c 	new file:   0-main.c 	new file:   README.md 	new file:   print_array.c 	new file:   print_list.c 	new file:   sort.h

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending
+ * order using the [Bubble sort] algorithm
+ *
+ * @array: The array of integers to sort
+ * @size: The size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0, sz = size;
+	int tmp = 0;
+
+	while (i < (sz - 1))
+	{
+		if (array[i] > array[i + 1])
+		{
+			tmp = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = tmp;
+			print_array(array, size);
+		}
+		else if ((i + 2) == sz)
+		{
+			i = 0;
+			sz--;
+		}
+		else
+		{
+			i++;
+		}
+	}
+}
